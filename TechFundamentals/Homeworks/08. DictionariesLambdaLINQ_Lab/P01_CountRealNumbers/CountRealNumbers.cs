58d7ca69a859b3a92eae0a6f907f96f48d3c63d5
Philip Shishov <philshishov@gmail.com>
2019-06-26 00:02:25 +0200

    All Problems - Done

---
+++ TechFundamentals/Homeworks/08. DictionariesLambdaLINQ_Lab/P01_CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,37 @@
+﻿
+namespace P01_CountRealNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class CountRealNumbers
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (int number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
