b6326639aa0986c31bd6906d62357f4aa13b79fe
Philip Shishov <philshishov@gmail.com>
2019-06-27 08:18:39 +0200

    All Problems - Done

---
+++ TechFundamentals/Homeworks/09. ObjectsandClasses_Lab/P05_Students/Students.cs
@@ -0,0 +1,98 @@
+﻿
+namespace P05_Students
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Students
+    {
+        public class Student
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string HomeTown { get; set; }
+        }
+
+        static void Main()
+        {
+            List<Student> students = new List<Student>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                string[] stuInformation = input.Split();
+
+                string firstName = stuInformation[0];
+                string lastName = stuInformation[1];
+                int age = int.Parse(stuInformation[2]);
+                string hometown = stuInformation[3];
+
+                if (IsStudentExisting(students, firstName, lastName))
+                {
+                    Student student = GetStudent(students, firstName, lastName);
+
+                    student.FirstName = firstName;
+                    student.LastName = lastName;
+                    student.Age = age;
+                    student.HomeTown = hometown;
+                }
+
+                else
+                {
+                    var student = new Student()
+                    {
+                        FirstName = firstName,
+                        LastName = lastName,
+                        Age = age,
+                        HomeTown = hometown
+                    };
+
+                    students.Add(student);
+
+                }
+            }
+
+            string filterCity = Console.ReadLine();
+
+            foreach (var student in students.Where(stu => stu.HomeTown == filterCity))
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+
+        static Student GetStudent(List<Student> students, string firstName, string lastName)
+        {
+            Student existingStudent = null;
+
+            foreach (var student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    existingStudent = student;
+                }
+            }
+
+            return existingStudent;
+        }
+
+        static bool IsStudentExisting(List<Student> students, string firstName, string lastName)
+        {
+            foreach (var student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ TechFundamentals/Homeworks/09. ObjectsandClasses_Lab/P06_Students2/Students2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P06_Students2
+{
+    class Students2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
