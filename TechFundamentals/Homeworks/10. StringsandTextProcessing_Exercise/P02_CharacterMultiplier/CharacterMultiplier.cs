17df3a9a0833a45303071e063e46929d43d86ce4
Philip Shishov <philshishov@gmail.com>
2019-06-28 22:18:50 +0200

    All Problems - Done

---
+++ TechFundamentals/Homeworks/10. StringsandTextProcessing_Exercise/P02_CharacterMultiplier/CharacterMultiplier.cs
@@ -0,0 +1,50 @@
+﻿
+namespace P02_CharacterMultiplier
+{
+    using System;
+
+    class CharacterMultiplier
+    {
+        static void Main()
+        {
+
+            string[] words = Console.ReadLine()
+                .Split();
+
+            string firstWord = words[0];
+            string secondWord = words[1];
+
+            int minLenght = Math.Min(firstWord.Length, secondWord.Length);
+            int totalSum = 0;
+
+            for (int i = 0; i < minLenght; i++)
+            {
+                totalSum += firstWord[i] * secondWord[i];
+            }
+
+            if (firstWord.Length > secondWord.Length)
+            {
+                totalSum += CalculateMissingCharacters(minLenght, firstWord);
+            }
+
+            else
+            {
+                totalSum += CalculateMissingCharacters(minLenght, secondWord);
+            }
+
+            Console.WriteLine(totalSum);
+        }
+
+        private static int CalculateMissingCharacters(int lenght, string word)
+        {
+            int totalSum = 0;
+
+            for (int i = lenght; i < word.Length; i++)
+            {
+                totalSum += word[i];
+            }
+
+            return totalSum;
+        }
+    }
+}
