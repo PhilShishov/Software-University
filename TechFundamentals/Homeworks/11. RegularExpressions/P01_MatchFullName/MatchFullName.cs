90b6adf5c0b4cd96875af4c51974267829a7c83c
Philip Shishov <philshishov@gmail.com>
2019-06-29 09:45:45 +0200

    All Problems - Done

---
+++ TechFundamentals/Homeworks/11. RegularExpressions/P01_MatchFullName/MatchFullName.cs
@@ -0,0 +1,25 @@
+﻿
+namespace P01_MatchFullName
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class MatchFullName
+    {
+        static void Main()
+        {
+            string names = Console.ReadLine();            
+
+            string regexPattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            MatchCollection matchedNames = Regex.Matches(names, regexPattern);
+
+            foreach (Match name in matchedNames)
+            {
+                Console.Write(name.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
