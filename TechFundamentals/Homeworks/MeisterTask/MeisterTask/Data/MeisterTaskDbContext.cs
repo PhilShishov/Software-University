78da29f94c5a97576ebbfb500aa9f6af1f0528b8
Philip Shishov <philshishov@gmail.com>
2019-07-03 08:39:14 +0200

    All Problems - Done

---
+++ TechFundamentals/Homeworks/MeisterTask/MeisterTask/Data/MeisterTaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MeisterTask.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using MeisterTask.Models;
+
+    public class MeisterTaskDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=MeisterTaskDb;Integrated Security = true;");
+        }
+    }
+}
