1a184b507745ff8edc5e3826cd85f7ffb979d8b7
Philip Shishov <philshishov@gmail.com>
2019-07-09 08:57:46 +0200

    All Problems - Done

---
+++ TechFundamentals/Exams/MidExam/P03_BakingFactory/BakingFactory.cs
@@ -0,0 +1,54 @@
+﻿
+namespace P03_BakingFactory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class BakingFactory
+    {
+        static void Main()
+        {
+            var resultBatch = new List<int>();
+
+            string input = Console.ReadLine();
+
+            int sumBatch = 0;
+            int bestSumBatch = int.MinValue;
+
+            while (input != "Bake It!")
+            {
+                var currentBatch = input
+                    .Split("#")
+                    .Select(int.Parse)
+                    .ToList();
+
+                sumBatch = currentBatch.Sum();
+
+                if (sumBatch > bestSumBatch)
+                {
+                    bestSumBatch = sumBatch;
+                    resultBatch = currentBatch;
+                }
+
+                else if (sumBatch == bestSumBatch)
+                {
+                    if ((sumBatch / currentBatch.Count) > (sumBatch / resultBatch.Count))
+                    {
+                        resultBatch = currentBatch;
+                    }
+                    else if ((sumBatch / currentBatch.Count == sumBatch / resultBatch.Count)
+                        && currentBatch.Count < resultBatch.Count)
+                    {
+                        resultBatch = currentBatch;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Best Batch quality: {bestSumBatch}");
+            Console.WriteLine(string.Join(" ", resultBatch));
+        }
+    }
+}
---
+++ TechFundamentals/Exams/MidExam/P03_QuestsJournal/QuestsJournal.cs
@@ -0,0 +1,58 @@
+﻿
+namespace P03_QuestsJournal
+{
+    using System;
+    using System.Linq;
+
+    class QuestsJournal
+    {
+        static void Main()
+        {
+            var resultQuests = Console.ReadLine().Split(", ").ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "Retire!")
+            {
+                var quests = input.Split(" - ");
+
+                string command = quests[0];
+                string quest = quests[1];
+
+                if (command == "Start")
+                {
+                    if (!resultQuests.Contains(quest))
+                    {
+                        resultQuests.Add(quest);
+                    }
+                }
+                else if (command == "Complete")
+                {
+                    resultQuests.Remove(quest);
+                }
+                else if (command == "Side Quest")
+                {
+                    var sideQuest = quest.Split(":");
+                    if (resultQuests.Contains(sideQuest[0]) && !resultQuests.Contains(sideQuest[1]))
+                    {
+                        int index = resultQuests.IndexOf(sideQuest[0]) + 1;
+                        //int index = resultQuests.FindIndex(x => x == sideQuest[0]);
+                        resultQuests.Insert(index, sideQuest[1]);
+                    }
+                }
+                else if (command == "Renew")
+                {
+                    if (resultQuests.Contains(quest))
+                    {
+                        resultQuests.Remove(quest);
+                        resultQuests.Add(quest);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", resultQuests));
+        }
+    }
+}
