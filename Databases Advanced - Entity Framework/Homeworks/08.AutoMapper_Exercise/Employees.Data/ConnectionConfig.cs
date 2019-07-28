ac90190640e9fad10c1885843c7daaa1331818d2
Philip Shishov <philshishov@gmail.com>
2019-07-28 07:20:54 +0300

    All Problems - Done

---
+++ Databases Advanced - Entity Framework/Homeworks/08.AutoMapper_Exercise/Employees.Data/ConnectionConfig.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Employees.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ConnectionConfig
+    {
+        public const string ConnectionString =
+            "Server=.\\SQLEXPRESS;Database=EmployeesDb;Integrated Security=True;";
+    }
+}
---
+++ Databases Advanced - Entity Framework/Homeworks/08.AutoMapper_Exercise/Employees.Data/EmployeesContext.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Employees.Data
+{
+    using Employees.Data.EntityConfigurations;
+    using Employees.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class EmployeesContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConnectionConfig.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new EmployeeConfig());
+        }
+    }
+}
