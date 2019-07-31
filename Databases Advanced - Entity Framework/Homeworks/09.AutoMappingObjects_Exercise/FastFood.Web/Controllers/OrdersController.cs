c2cde85b699e6117fb660db13aac98753ea4b197
Philip Shishov <philshishov@gmail.com>
2019-07-31 09:39:53 +0300

    All Problems - Done

---
+++ Databases Advanced - Entity Framework/Homeworks/09.AutoMappingObjects_Exercise/FastFood.Web/Controllers/OrdersController.cs
@@ -0,0 +1,87 @@
+﻿namespace FastFood.Web.Controllers
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using FastFood.Models;
+    using FastFood.Models.Enums;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Linq;
+    using ViewModels.Orders;
+
+    public class OrdersController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public OrdersController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            var viewOrder = new CreateOrderViewModel
+            {
+                Items = this.context.Items.Select(x => x.Name).ToList(),
+                Employees = this.context.Employees.Select(x => x.Name).ToList(),
+            };
+
+            return this.View(viewOrder);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateOrderInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.RedirectToAction("Error", "Home");
+            }
+
+            var order = this.mapper.Map<Order>(model);
+            order.DateTime = DateTime.Now;
+
+            var item = this.context
+               .Items
+               .FirstOrDefault(x => x.Name == model.ItemName);
+
+            var employee = this.context
+               .Employees
+               .FirstOrDefault(x => x.Name == model.EmployeeName);
+
+            if (item != null)
+            {
+                order.OrderItems.Add(new OrderItem()
+                {
+                    ItemId = item.Id,
+                    Order = order,
+                    Quantity = model.Quantity
+                });
+            }
+
+            order.Type = Enum.Parse<OrderType>(model.OrderType);
+
+            if (employee != null)
+            {
+                order.EmployeeId = employee.Id;
+            }            
+
+            this.context.Orders.Add(order);
+            this.context.SaveChanges();
+
+            return this.RedirectToAction("All", "Orders");
+        }
+
+        public IActionResult All()
+        {
+            var orders = this.context
+               .Orders
+               .ProjectTo<OrderAllViewModel>(mapper.ConfigurationProvider)
+               .ToList();
+
+            return this.View(orders);
+        }
+    }
+}
---
+++ Databases Advanced - Entity Framework/Homeworks/09.AutoMappingObjects_Exercise/FastFood.Web/ViewModels/Orders/CreateOrderInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastFood.Web.ViewModels.Orders
+{
+    public class CreateOrderInputModel
+    {
+        [Required]
+        [MinLength(3), MaxLength(30)]
+        public string Customer { get; set; }
+
+        public string ItemName { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        [Range(1, 100)]
+        public int Quantity { get; set; }
+
+        public string OrderType { get; set; }
+    }
+}
