923d8af47a0c180b303d24407f3541b173cb864b
Philip Shishov <philshishov@gmail.com>
2019-08-03 11:02:05 +0300

    All Problems - Done

---
+++ Databases Advanced - Entity Framework/Homeworks/CustomAutoMapper/CustomAutoMapper/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomAutoMapper
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Address address { get; set; }
+    }
+}
---
+++ Databases Advanced - Entity Framework/Homeworks/CustomAutoMapper/CustomAutoMapper/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace CustomAutoMapper
+{
+    using Newtonsoft.Json;
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person()
+            {
+                FirstName = "Pesho",
+                LastName = "Peshov",
+                address = new Address()
+                {
+                    City = "Sofia",
+                    Street = "Vitosha",
+                    Number = 1
+                }
+            };
+
+            var mapper = new Mapper();
+
+            var student = mapper.Map<Student>(person);
+
+            Console.WriteLine(JsonConvert.SerializeObject(student));
+        }
+    }
+}
