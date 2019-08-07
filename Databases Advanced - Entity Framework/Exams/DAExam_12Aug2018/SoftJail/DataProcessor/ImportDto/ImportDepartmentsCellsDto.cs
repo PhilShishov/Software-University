a7bffb72dbb310852cc6eae9a6488fed3e95989e
Philip Shishov <philshishov@gmail.com>
2019-08-07 15:15:00 +0300

    All Problems - Done

---
+++ Databases Advanced - Entity Framework/Exams/DAExam_12Aug2018/SoftJail/DataProcessor/ImportDto/ImportDepartmentsCellsDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class ImportDepartmentsCellsDto
+    {
+        [MinLength(3), MaxLength(25)]
+        public string Name { get; set; }
+
+        public CellDto[] Cells { get; set; }
+
+    }
+
+    public class CellDto
+    {
+        [Range(1, 1000)]
+        public int CellNumber { get; set; }
+
+        [Required]
+        public bool HasWindow { get; set; }
+    }
+}
---
+++ Databases Advanced - Entity Framework/Exams/DAExam_12Aug2018/SoftJail/DataProcessor/ImportDto/PrisonerDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Prisoner")]
+    public class PrisonerDto
+    {
+        [XmlAttribute("id")]
+        public int PrisonerId { get; set; }
+    }
+}
