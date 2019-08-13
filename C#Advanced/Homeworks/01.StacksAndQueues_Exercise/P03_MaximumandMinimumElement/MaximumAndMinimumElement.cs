d22066eb66a60ae93344feb0ff1f704e35209986
Philip Shishov <philshishov@gmail.com>
2019-08-13 08:43:37 +0300

    All Problems - Done

---
+++ C#Advanced/Homeworks/01.StacksAndQueues_Exercise/P03_MaximumandMinimumElement/MaximumAndMinimumElement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P03_MaximumandMinimumElement
+{
+    class MaximumAndMinimumElement
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder result = new StringBuilder();
+
+            Stack<int> integers = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int currentOperation = tokens[0];
+
+                if (currentOperation == 1)
+                {
+                    integers.Push(tokens[1]);
+                }
+
+                else if (currentOperation == 2 && integers.Count > 0)
+                {
+                    integers.Pop();
+                }
+
+                else if (currentOperation == 3 && integers.Count > 0)
+                {
+                    result.Append($"{integers.Max()}{Environment.NewLine}");
+                }
+
+                else if (currentOperation == 4 && integers.Count > 0)
+                {
+                    result.Append($"{integers.Min()}{Environment.NewLine}");
+                }
+            }
+
+            result.AppendJoin(", ", integers);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C#Advanced/Homeworks/01.StacksAndQueues_Exercise/P07_TruckTour/StartUp.cs
@@ -0,0 +1,56 @@
+﻿
+namespace P07_TruckTour
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Queue<int[]> petrolPumps = new Queue<int[]>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int[] petrolPump = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                petrolPumps.Enqueue(petrolPump);
+            }
+
+            int index = 0;
+
+            while (true)
+            {
+                int totalFuel = 0;
+
+                foreach (var petrolPump in petrolPumps)
+                {
+                    int amount = petrolPump[0];
+                    int distance = petrolPump[1];
+
+                    totalFuel += amount - distance;
+
+                    if (totalFuel < 0)
+                    {
+                        petrolPumps.Enqueue(petrolPumps.Dequeue());
+                        index++;
+                        break;
+                    }
+                }
+
+                if (totalFuel >= 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(index);
+        }
+    }
+}
