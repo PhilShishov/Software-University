6443f0cb97026cc2871903cc4fbc0568a9f21a88
Philip Shishov <philshishov@gmail.com>
2019-08-17 09:17:31 +0200

    All Problems - Done

---
+++ C#Advanced/Homeworks/03.SetsAndDictionariesAdvanced_Exercise/P01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                set.Add(name);
+
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, set));
+
+        }
+    }
+}
---
+++ C#Advanced/Homeworks/03.SetsAndDictionariesAdvanced_Exercise/P02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = nums[0];
+            int m = nums[1];
+
+            HashSet<int> setOne = new HashSet<int>();
+            HashSet<int> setTwo = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                setOne.Add(num);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                setTwo.Add(num);
+            }
+
+            setOne.IntersectWith(setTwo);
+
+            Console.WriteLine(string.Join(" ", setOne));
+        }
+    }
+}
