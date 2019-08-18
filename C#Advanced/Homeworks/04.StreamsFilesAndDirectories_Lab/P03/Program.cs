a7901963583a235a1b2da972a0a3ddaea159be90
Philip Shishov <philshishov@gmail.com>
2019-08-18 11:39:33 +0200

    All Problems - Done

---
+++ C#Advanced/Homeworks/04.StreamsFilesAndDirectories_Lab/P03/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace P03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> listOfWords = new Dictionary<string, int>();
+
+            string wordsPath = Path.Combine("Files", "words.txt");
+            string textPath = Path.Combine("Files", "text.txt");
+
+            using (var reader = new StreamReader(wordsPath))
+            {
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    string[] words = line.Split();
+                    foreach (var word in words)
+                    {
+                        if (!listOfWords.ContainsKey(word))
+                        {
+                            listOfWords.Add(word.ToLower(), 0);
+                        }
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            using (var reader = new StreamReader(textPath))
+            {
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    string[] words = line.Split(new char[] { ' ', '-', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var word in words)
+                    {
+                        if (listOfWords.ContainsKey(word.ToLower()))
+                        {
+                            listOfWords[word.ToLower()]++;
+                        }
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            StreamWriter writer = new StreamWriter(Path.Combine("Files", "Output.txt"));
+
+            using (writer)
+            {
+
+                foreach (var word in listOfWords.OrderByDescending(w => w.Value))
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+        }
+    }
+}
