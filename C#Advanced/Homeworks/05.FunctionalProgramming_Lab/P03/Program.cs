8a81bac6adb909755bdd5fd26bac3ff71fe7c8f5
Philip Shishov <philshishov@gmail.com>
2019-08-19 13:16:51 +0200

    All Problems - Done

---
+++ C#Advanced/Homeworks/05.FunctionalProgramming_Lab/P03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> isUpper = w => w[0] == w.ToUpper()[0];
+
+            var text = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(isUpper);
+
+            Console.WriteLine(string.Join(Environment.NewLine, text));
+        }
+    }
+}
