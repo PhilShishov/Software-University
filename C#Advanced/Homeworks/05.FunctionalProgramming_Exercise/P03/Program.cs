f5d041c4080bcb478a2b7e4f0a339adcb7080b48
Philip Shishov <philshishov@gmail.com>
2019-08-20 08:03:15 +0200

    All Problems - Done

---
+++ C#Advanced/Homeworks/05.FunctionalProgramming_Exercise/P03/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace P03
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> minNum = n => nums.Min();
+
+            Console.WriteLine(minNum(nums));
+        }
+    }
+}
---
+++ C#Advanced/Homeworks/05.FunctionalProgramming_Exercise/P06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace P06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Func<int[], int[]> reverseFunc = num => nums.Reverse().ToArray();
+            Func<int[], int[]> removeFunc = num => nums.Where(x => x % n != 0).ToArray();
+            Action<int[]> printNums = numbers => Console.WriteLine(string.Join(" ", numbers));
+
+            nums = reverseFunc(nums);
+            nums = removeFunc(nums);
+            printNums(nums);            
+        }
+    }
+}
