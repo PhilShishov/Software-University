7ebbb9527fd02eb9c9eac55c6fb6391cae768893
Philip Shishov <philshishov@gmail.com>
2019-08-22 08:06:05 +0200

    All Problems - Done

---
+++ C#Advanced/Homeworks/07.Generics _Lab/BoxOfT/Box.cs
@@ -0,0 +1,39 @@
+﻿namespace BoxOfT
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public int Count => this.data.Count;
+
+        public void Add(T element)
+        {
+            this.data.Add(element);
+        }
+
+        public T Remove()
+        {
+            if (this.Count > 0)
+            {
+                T last = this.data.Last();
+                this.data.RemoveAt(Count - 1);
+
+                return last;
+            }
+
+            throw new InvalidOperationException("Cannot remove element from empty collection");
+
+        }
+    }
+}
---
+++ C#Advanced/Homeworks/07.Generics _Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericScale
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    public class EqualityScale<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public bool AreEqual()
+        {
+            return this.left.Equals(this.right);
+        }
+    }
+}
