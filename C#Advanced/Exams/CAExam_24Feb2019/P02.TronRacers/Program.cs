a3742062c9bedcd85d3da6c520c838717ba13071
Philip Shishov <philshishov@gmail.com>
2019-08-30 21:59:07 +0200

    All Problems - Done

---
+++ C#Advanced/Exams/CAExam_24Feb2019/P02.TronRacers/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace P02.TronRacers
+{
+    class Program
+    {
+        static char[][] tronField;
+
+        static int firstPlayerRow;
+        static int firstPlayerCol;
+
+        static int secondPlayerRow;
+        static int secondPlayerCol;
+
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            tronField = new char[rows][];
+
+            Initialize(tronField);
+
+            while (true)
+            {
+                var commands = Console.ReadLine().Split();
+
+                string commandFirstPlayer = commands[0];
+                string commandSecondPlayer = commands[1];
+
+                MoveFirstPlayer(commandFirstPlayer);
+
+                MoveSecondPlayer(commandSecondPlayer);
+
+                if (tronField[firstPlayerRow][firstPlayerCol] == 's')
+                {
+                    tronField[firstPlayerRow][firstPlayerCol] = 'x';
+                    End();
+                }
+                else
+                {
+                    tronField[firstPlayerRow][firstPlayerCol] = 'f';
+                }
+
+                if (tronField[secondPlayerRow][secondPlayerCol] == 'f')
+                {
+                    tronField[secondPlayerRow][secondPlayerCol] = 'x';
+                    End();
+                }
+                else
+                {
+                    tronField[secondPlayerRow][secondPlayerCol] = 's';
+                }
+            }
+        }
+
+        private static void MoveSecondPlayer(string commandSecondPlayer)
+        {
+            if (commandSecondPlayer == "up")
+            {
+                secondPlayerRow--;
+
+                if (secondPlayerRow < 0)
+                {
+                    secondPlayerRow = tronField.Length - 1;
+                }
+            }
+            else if (commandSecondPlayer == "down")
+            {
+                secondPlayerRow++;
+
+                if (secondPlayerRow > tronField.Length - 1)
+                {
+                    secondPlayerRow = 0;
+                }
+            }
+            else if (commandSecondPlayer == "left")
+            {
+                secondPlayerCol--;
+
+                if (secondPlayerCol < 0)
+                {
+                    secondPlayerCol = tronField[secondPlayerRow].Length - 1;
+                }
+            }
+            else if (commandSecondPlayer == "right")
+            {
+                secondPlayerCol++;
+
+                if (secondPlayerCol > tronField[secondPlayerRow].Length - 1)
+                {
+                    secondPlayerCol = 0;
+                }
+            }
+        }
+
+        private static void MoveFirstPlayer(string commandFirstPlayer)
+        {
+            if (commandFirstPlayer == "up")
+            {
+                firstPlayerRow--;
+
+                if (firstPlayerRow < 0)
+                {
+                    firstPlayerRow = tronField.Length - 1;
+                }
+            }
+            else if (commandFirstPlayer == "down")
+            {
+                firstPlayerRow++;
+
+                if (firstPlayerRow > tronField.Length - 1)
+                {
+                    firstPlayerRow = 0;
+                }
+            }
+            else if (commandFirstPlayer == "left")
+            {
+                firstPlayerCol--;
+
+                if (firstPlayerCol < 0)
+                {
+                    firstPlayerCol = tronField[firstPlayerRow].Length - 1;
+                }
+            }
+            else if (commandFirstPlayer == "right")
+            {
+                firstPlayerCol++;
+
+                if (firstPlayerCol > tronField[firstPlayerRow].Length - 1)
+                {
+                    firstPlayerCol = 0;
+                }
+            }
+        }
+
+        private static void End()
+        {
+            for (int row = 0; row < tronField.Length; row++)
+            {
+                for (int col = 0; col < tronField[row].Length; col++)
+                {
+                    Console.Write(tronField[row][col]);
+                }
+                Console.WriteLine();
+            }
+            Environment.Exit(0);
+        }
+
+        private static void Initialize(char[][] tronField)
+        {
+            for (int row = 0; row < tronField.Length; row++)
+            {
+                var input = Console.ReadLine().ToCharArray();
+                tronField[row] = new char[input.Length];
+
+                for (int col = 0; col < input.Length; col++)
+                {
+                    tronField[row][col] = input[col];
+
+                    if (tronField[row][col] == 'f')
+                    {
+                        firstPlayerRow = row;
+                        firstPlayerCol = col;
+                    }
+                    if (tronField[row][col] == 's')
+                    {
+                        secondPlayerRow = row;
+                        secondPlayerCol = col;
+                    }
+
+                }
+            }
+        }
+    }
+}
+
---
+++ C#Advanced/Exams/CAExam_24Feb2019/P03. Heroes/HeroRepository.cs
@@ -0,0 +1,65 @@
+﻿namespace Heroes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+
+    public class HeroRepository
+    {
+        private IList<Hero> heroes;
+
+        public HeroRepository()
+        {
+            this.heroes = new List<Hero>();
+        }
+
+        public int Count => this.heroes.Count;
+
+        public void Add(Hero hero)
+        {
+            this.heroes.Add(hero);
+        }
+
+        public void Remove(string name)
+        {
+            var hero = this.heroes.FirstOrDefault(h => h.Name == name);
+
+            this.heroes.Remove(hero);
+        }
+
+        public Hero GetHeroWithHighestStrength()
+        {
+            var hero = this.heroes.OrderByDescending(h => h.Item.Strength).FirstOrDefault();
+
+            return hero;
+        }
+
+        public Hero GetHeroWithHighestAbility()
+        {
+            var hero = this.heroes.OrderByDescending(h => h.Item.Ability).FirstOrDefault();
+
+            return hero;
+        }
+
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            var hero = this.heroes.OrderByDescending(h => h.Item.Intelligence).FirstOrDefault();
+
+            return hero;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var hero in this.heroes)
+            {
+                sb.AppendLine(hero.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
