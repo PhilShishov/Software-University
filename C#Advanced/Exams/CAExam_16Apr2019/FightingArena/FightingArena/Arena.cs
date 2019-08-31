3938a15971ac8825ed8bd6aa72cb47dc966c5848
Philip Shishov <philshishov@gmail.com>
2019-08-31 08:23:28 +0200

    All Problems - Done

---
+++ C#Advanced/Exams/CAExam_16Apr2019/FightingArena/FightingArena/Arena.cs
@@ -0,0 +1,62 @@
+﻿namespace FightingArena
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+
+    public class Arena
+    {
+        private List<Gladiator> gladiators;
+
+        public Arena(string name)
+        {
+            this.gladiators = new List<Gladiator>();
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public int Count => this.gladiators.Count;
+
+        public void Add(Gladiator gladiator)
+        {
+            gladiators.Add(gladiator);
+        }
+
+        public void Remove(string name)
+        {
+            var gladiator = gladiators.Where(g => g.Name == name).FirstOrDefault();
+            gladiators.Remove(gladiator);
+        }
+
+        public Gladiator GetGladitorWithHighestStatPower()
+        {
+            var gladiator = gladiators.OrderByDescending(g => g.GetStatPower()).FirstOrDefault();
+            return gladiator;
+        }
+
+        public Gladiator GetGladitorWithHighestWeaponPower()
+        {
+            var gladiator = gladiators.OrderByDescending(g => g.GetWeaponPower()).FirstOrDefault();
+            return gladiator;
+        }
+
+        public Gladiator GetGladitorWithHighestTotalPower()
+        {
+            var gladiator = gladiators.OrderByDescending(g => g.GetTotalPower()).FirstOrDefault();
+            return gladiator;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.Name} - {this.Count} gladiators are participating.");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+}
---
+++ C#Advanced/Exams/CAExam_16Apr2019/P01.TrojanInvasion/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.TrojanInvasion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int waves = int.Parse(Console.ReadLine());
+
+            var platesInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            List<int> plates = new List<int>(platesInput);
+            Stack<int> warriors = new Stack<int>();
+
+            for (int wave = 1; wave <= waves; wave++)
+            {
+                var warriorsInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+                foreach (var warrior in warriorsInput)
+                {
+                    warriors.Push(warrior);
+                }
+
+                if (wave % 3 == 0)
+                {
+                    int additionalPlate = int.Parse(Console.ReadLine());
+
+                    plates.Add(additionalPlate);
+                }
+
+                while (warriors.Count > 0 && plates.Count > 0)
+                {
+                    int currentPlate = plates[0];
+                    int currentWarrior = warriors.Pop();
+
+                    if (currentWarrior > currentPlate)
+                    {
+                        currentWarrior -= currentPlate;
+                        warriors.Push(currentWarrior);
+                        plates.RemoveAt(0);
+                    }
+                    else if (currentWarrior < currentPlate)
+                    {
+                        currentPlate -= currentWarrior;
+                        plates[0] = currentPlate;
+                    }
+                    else if (currentWarrior == currentPlate)
+                    {
+                        plates.RemoveAt(0);
+                    }
+                }
+
+                if (plates.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            if (plates.Count > 0)
+            {
+                Console.WriteLine($"The Spartans successfully repulsed the Trojan attack.");
+                Console.WriteLine($"Plates left: {string.Join(", ", plates)}");
+            }
+            else
+            {
+                Console.WriteLine($"The Trojans successfully destroyed the Spartan defense.");
+                Console.WriteLine($"Warriors left: {string.Join(", ", warriors)}");
+            }
+        }
+    }
+}
