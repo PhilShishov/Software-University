45a81c7a7ddef8bd30fcaf9aa6966d7bdc30c001
Philip Shishov <philshishov@gmail.com>
2019-09-01 19:50:57 +0200

    All Problems - Done

---
+++ C#Advanced/Exams/CAExam_23June2019/P02/Astronaut.cs
@@ -0,0 +1,23 @@
+﻿namespace SpaceStationRecruitment
+{
+    public class Astronaut
+    {
+        public Astronaut(string name, int age, string country)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"Astronaut: {this.Name}, {this.Age} ({this.Country})";
+        }
+    }
+}
