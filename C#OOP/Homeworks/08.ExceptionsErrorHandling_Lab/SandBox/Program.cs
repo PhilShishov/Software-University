5da1fbf903b8e6364b0301c9388bac3ad9138710
Philip Shishov <philshishov@gmail.com>
2019-09-19 09:26:01 +0200

    All Problems - Done

---
+++ C#OOP/Homeworks/08.ExceptionsErrorHandling_Lab/SandBox/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("cat");
+            Console.WriteLine();
+        }
+
+        static void Print(string s)
+        {
+            if (s.Length <= 1)
+            {
+                Console.Write(s);
+            }
+            else
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    string a = s.Substring(i + 1);
+                    string b = s.Substring(0, i);
+                    Print(a + b);
+                }
+
+            }
+        }
+    }
+}
