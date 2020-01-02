18cb4f5462e3f39d55b11cf1ed53fbb29e2429ae
Philip Shishov <philshishov@gmail.com>
2020-01-02 12:46:40 +0100

    Object Composition last problem solved

--- JS Advanced/Homeworks/05.ObjectComposition_Lab/P05.js
+++ JS Advanced/Homeworks/05.ObjectComposition_Lab/P05.js
@@ -1,26 +1,61 @@
-function sum() {
-    return (function () {
-        let selector1;
-        let selector2;
-        let resultSelector;
 
-        return {
-            init: (s1, s2, result) => {
-                selector1 = s1.slice(1);
-                selector2 = s2.slice(1);
-                resultSelector = result.slice(1);
-            },
-            add: () => {
-                let value1 = +document.getElementById(selector1).value;
-                let value2 = +document.getElementById(selector2).value;
-                document.getElementById(resultSelector).value = value1 + value2;
-            },
-            subtract: () => {
-                let value1 = +document.getElementById(selector1).value;
-                let value2 = +document.getElementById(selector2).value;
-
-                document.getElementById(resultSelector).value = value1 - value2;
-            }
-        };
-    })();
+const SELECTORS = {
+    NUM1: '#num1',
+    NUM2: '#num2',
+    RESULT: '#result',
+    SUM_BUTTON: '#sumButton',
+    SUBTRACT_BUTTON: '#subtractButton',
 }
+
+class Calculator {
+    constructor() {
+        // debugger;
+        this.handleAddClick = this.handleAddClick.bind(this);
+        this.handleSubtractClick = this.handleSubtractClick.bind(this);
+
+        this.num1 = document.querySelector(SELECTORS.NUM1);
+        this.num2 = document.querySelector(SELECTORS.NUM2);
+        this.result = document.querySelector(SELECTORS.RESULT);
+        this.btnAdd = document.querySelector(SELECTORS.SUM_BUTTON);
+        this.btnSubstract = document.querySelector(SELECTORS.SUBTRACT_BUTTON);
+
+        this._initEvents();
+    }
+
+    handleAddClick() {
+        const x = this.num1.value;
+        const y = this.num2.value;
+        this.result.value = +x + +y;
+    }
+
+    handleSubtractClick() {
+        const x = this.num1.value;
+        const y = this.num2.value;
+        this.result.value = x - y;
+    }
+
+    _initEvents() {
+        this.btnAdd.addEventListener('click', this.handleAddClick);
+        this.btnSubstract.addEventListener('click', this.handleSubtractClick);
+    }
+}
+
+new Calculator();
+
+
+// function sum() {
+//     let firstElement;
+//     let secondElement;
+//     let resultElement;
+
+//     return {
+//         init: (selectorOne, selectorTwo, resultSelector) => {
+//             firstElement = $(selectorOne);
+//             secondElement = $(selectorTwo);
+//             resultElement = $(resultSelector);
+//         },
+
+//         add: () => resultElement.val(+firstElement.val() + +secondElement.val()),
+//         subtract: () => resultElement.val(+firstElement.val() - +secondElement.val())
+//     }
+// }
