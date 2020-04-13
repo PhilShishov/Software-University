1c38945fa51ec2a76c0c94820e8a0c30e819ec34
Philip Shishov <philipshishov@gmail.com>
2020-04-13 11:19:02 +0200

    All Problems Done

---
+++ JS Apps/Homeworks/04.RoutingArchitecture_Exercise/Skeleton/controllers/homeController.js
@@ -0,0 +1,24 @@
+const homeController = function () {
+
+    const getHome = function (context) {
+
+        const loggedIn = storage.getData('userInfo') !== null;
+        
+        if(loggedIn){
+            const username = JSON.parse(storage.getData('userInfo')).username;
+            context.loggedIn = loggedIn;
+            context.username = username;
+        }
+
+        context.loadPartials({
+            header: "../views/common/header.hbs",
+            footer: "../views/common/footer.hbs"
+        }).then(function(){
+            this.partial('../views/home/home.hbs')
+        })
+    };
+
+    return {
+        getHome
+    }
+}();
