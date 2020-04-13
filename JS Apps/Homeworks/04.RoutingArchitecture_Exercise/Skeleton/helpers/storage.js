d6b484e0eff7a6aef93018a2295efe4d7513aafb
Philip Shishov <philshishov@gmail.com>
2020-04-13 22:11:32 +0200

    All Problems Done

---
+++ JS Apps/Homeworks/04.RoutingArchitecture_Exercise/Skeleton/helpers/storage.js
@@ -0,0 +1,32 @@
+const storage = function () {
+
+    const appKey = "YOUR APP KEY";
+    const appSecret = "YOUR APP SECRET";
+
+    const getData = function (key) {
+        return localStorage.getItem(key + appKey);
+    };
+
+    const saveData = function (key, value) {
+        localStorage.setItem(key + appKey, JSON.stringify(value));
+    };
+
+    const saveUser = function (data) {
+        saveData("userInfo", data);
+        saveData("authToken", data._kmd.authtoken);
+    };
+
+    const deleteUser = function () {
+        localStorage.removeItem("userInfo" + appKey);
+        localStorage.removeItem("authToken" + appKey);
+    };
+
+    return {
+        getData,
+        saveData,
+        saveUser,
+        deleteUser,
+        appKey,
+        appSecret
+    }
+}();
