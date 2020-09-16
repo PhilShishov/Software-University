7daa9006dfd007be83512bf619392ba1458777ab
Philip Shishov <philshishov@gmail.com>
2020-09-16 21:17:41 +0200

    All Problems Done for Angular Lab 02

---
+++ Angular/Homeworks/02.Components_Lab/articles-app/src/app/data/data.ts
@@ -0,0 +1,13 @@
+
+export class ArticleData {
+    getData(): Article[] {
+        let articles: Article[] = [];
+
+        for (let i = 0; i < data.length; i++) {
+            articles[i] = new Article(data[i].title, data[i].description,
+                data[i].author, data[i].imageUrl);
+        }
+
+        return articles;
+    }
+}
---
+++ Angular/Homeworks/02.Components_Lab/articles-app/src/app/models/article.model.ts
@@ -0,0 +1,16 @@
+export class Article {
+    title: string;
+    description: string;
+    author: string;
+    imageUrl: string;
+
+    constructor(title: string,
+        description: string,
+        author: string,
+        imageUrl: string) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.imageUrl = imageUrl;
+    }
+}
