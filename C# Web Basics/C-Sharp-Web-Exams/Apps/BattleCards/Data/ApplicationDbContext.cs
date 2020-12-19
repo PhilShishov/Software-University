3fffa3b5f044e5c28c4fc148e69bba42c90e4ed4
Philip Shishov <philshishov@gmail.com>
2020-12-19 19:53:43 +0100

    Update Shared Trip Exam with SUS server and ASP.NET Core 3.1.9 - All problems done

---
+++ C# Web Basics/C-Sharp-Web-Exams/Apps/BattleCards/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleCards.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=BattleCards_Shishov;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
---
+++ C# Web Basics/C-Sharp-Web-Exams/Apps/BattleCards/Startup.cs
@@ -0,0 +1,21 @@
+﻿using BattleCards.Data;
+using Microsoft.EntityFrameworkCore;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System.Collections.Generic;
+
+namespace BattleCards
+{
+    public class Startup : IMvcApplication
+    {
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+           
+        }
+
+        public void Configure(List<Route> routeTable)
+        {
+            new ApplicationDbContext().Database.Migrate();
+        }
+    }
+}
