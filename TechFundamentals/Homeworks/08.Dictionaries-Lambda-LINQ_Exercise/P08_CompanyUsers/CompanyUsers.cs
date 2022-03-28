1993303615f7a7c41e01bc1ff3163126250419a9
Philip Shishov <philshishov@gmail.com>
2022-03-28 08:31:22 +0200

    Dictionaries exercise * update to net6

--- TechFundamentals/Homeworks/08.Dictionaries-Lambda-LINQ_Exercise/P08_CompanyUsers/CompanyUsers.cs
+++ TechFundamentals/Homeworks/08.Dictionaries-Lambda-LINQ_Exercise/P08_CompanyUsers/CompanyUsers.cs
@@ -3,50 +3,48 @@ namespace P08_CompanyUsers
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
 
     class CompanyUsers
     {
         static void Main()
         {
-            //var employees = new SortedDictionary<string, List<string>>();
+            var employees = new SortedDictionary<string, List<string>>();
 
-            //while (true)
-            //{
-            //    string command = Console.ReadLine();
-            //    if (command == "End")
-            //    {
-            //        break;
-            //    }
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                {
+                    break;
+                }
 
-            //    string[] tokens = command.Split(" -> ");
-            //    string companyName = tokens[0];
-            //    string employeeId = tokens[1];
+                string[] tokens = command.Split(" -> ");
+                string companyName = tokens[0];
+                string employeeId = tokens[1];
 
-            //    if (employees.ContainsKey(companyName) == false)
-            //    {
-            //        employees.Add(companyName, new List<string>());
-            //        employees[companyName].Add(employeeId);
-            //    }
-            //    else
-            //    {
-            //        if (employees[companyName].Contains(employeeId) == false)
-            //        {
-            //            employees[companyName].Add(employeeId);
-            //        }
-            //    }
-            //}
+                if (employees.ContainsKey(companyName) == false)
+                {
+                    employees.Add(companyName, new List<string>());
+                    employees[companyName].Add(employeeId);
+                }
+                else
+                {
+                    if (employees[companyName].Contains(employeeId) == false)
+                    {
+                        employees[companyName].Add(employeeId);
+                    }
+                }
+            }
 
-            //foreach (var kvp in employees)
-            //{
-            //    Console.WriteLine($"{kvp.Key}");
+            foreach (var kvp in employees)
+            {
+                Console.WriteLine($"{kvp.Key}");
 
-            //    foreach (var employee in kvp.Value)
-            //    {
-            //        Console.WriteLine($"-- {employee}");
-            //    }
-            //}
+                foreach (var employee in kvp.Value)
+                {
+                    Console.WriteLine($"-- {employee}");
+                }
+            }
 
         }
     }
