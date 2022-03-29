21a6db5773016a41fbabf827a796ed8aeb97beea
Philip Shishov <philshishov@gmail.com>
2022-03-29 08:55:50 +0200

    Add new P09 and P10

---
+++ TechFundamentals/Homeworks/08.Dictionaries-Lambda-LINQ_Exercise/P09_ForceBook/Program.cs
@@ -0,0 +1,80 @@
+﻿
+namespace P09_ForceBook
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, List<string>> forcebook = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Lumpawaroo")
+                {
+                    break;
+                }
+
+                if (command.Contains(" | "))
+                {
+                    string[] tokens = command.Split(new string[] { " | " }, StringSplitOptions.None);
+                    string side = tokens[0];
+                    string user = tokens[1];
+                    if (forcebook.Values.Any(x => x.Contains(user)) == false)
+                    {
+                        if (forcebook.ContainsKey(side) == false)
+                        {
+                            forcebook.Add(side, new List<string>());
+                        }
+                        forcebook[side].Add(user);
+                    }
+
+                }
+                else
+                {
+                    string[] tokens = command.Split(new string[] { " -> " }, StringSplitOptions.None);
+                    string side = tokens[1];
+                    string user = tokens[0];
+
+                    if (forcebook.Any(x => x.Value.Contains(user)))
+                    {
+                        foreach (var pair in forcebook)
+                        {
+                            if (pair.Value.Contains(user))
+                            {
+                                forcebook[pair.Key].Remove(user);
+                                break;
+                            }
+                        }
+                    }
+                    if (forcebook.ContainsKey(side))
+                    {
+                        forcebook[side].Add(user);
+                    }
+                    else
+                    {
+                        forcebook.Add(side, new List<string>() { user });
+                    }
+                    Console.WriteLine($"{user} joins the {side} side!");
+                }
+
+            }
+
+            foreach (var pair in forcebook.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                if (pair.Value.Count > 0)
+                {
+                    Console.WriteLine($"Side: {pair.Key}, Members: {pair.Value.Count}");
+                }
+                foreach (var user in pair.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"! {user}");
+                }
+            }
+        }
+    }
+}
---
+++ TechFundamentals/Homeworks/08.Dictionaries-Lambda-LINQ_Exercise/P10_SoftUniExamResults/Program.cs
@@ -0,0 +1,72 @@
+﻿
+namespace P10_SoftUniExamResults
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var participants = new Dictionary<string, int>();
+            var submissions = new Dictionary<string, int>(); ;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "exam finished")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split('-');
+
+                if (tokens.Length == 3)
+                {
+                    string name = tokens[0];
+                    string language = tokens[1];
+                    int points = int.Parse(tokens[2]);
+
+                    if (participants.ContainsKey(name) == false)
+                    {
+                        participants.Add(name, points);
+                    }
+                    else
+                    {
+                        if (participants[name] < points)
+                        {
+                            participants[name] = points;
+                        }
+                    }
+
+                    if (submissions.ContainsKey(language) == false)
+                    {
+                        submissions.Add(language, 0);
+                    }
+                    submissions[language]++;
+                }
+                else
+                {
+                    string name = tokens[0];
+                    if (participants.ContainsKey(name))
+                    {
+                        participants.Remove(name);
+                    }
+                }
+            }
+
+            Console.WriteLine("Results:");
+            foreach (var participant in participants.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{participant.Key} | {participant.Value}");
+            }
+
+            Console.WriteLine("Submissions:");
+            foreach (var submission in submissions.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{submission.Key} - {submission.Value}");
+            }
+        }
+    }
+}
