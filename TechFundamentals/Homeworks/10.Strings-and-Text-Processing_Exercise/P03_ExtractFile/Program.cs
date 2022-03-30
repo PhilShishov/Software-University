4b576615d79e614d619d7e8926e153e764805b73
Philip Shishov <philshishov@gmail.com>
2022-03-30 08:02:29 +0200

    Text processing Exercise * update to net6

---
+++ TechFundamentals/Homeworks/10.Strings-and-Text-Processing_Exercise/P03_ExtractFile/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace P03_ExtractFile
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    class ExtractFile
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split("\\")
+                .ToArray();
+
+            var last = input[^1];
+
+            // Solution 1 - Path
+            string fileName = Path.GetFileNameWithoutExtension(last);
+            string fileExt = Path.GetExtension(last).Replace(".", "");
+
+            //Solution 2 - Substring
+            //string fileExt = last.Substring(last.LastIndexOf('.') + 1);
+            //string fileName = last.Substring(0, last.LastIndexOf($".{fileExt}"));
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExt}");
+
+        }
+    }
+}
