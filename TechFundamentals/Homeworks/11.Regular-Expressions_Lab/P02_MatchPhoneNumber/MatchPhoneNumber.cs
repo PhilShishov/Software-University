eaf8e8028796ab34f2e40c976224489caf31bc1e
Philip Shishov <philshishov@gmail.com>
2022-04-01 08:01:51 +0200

    * RegEx Lab * net6 and refactor names

---
+++ TechFundamentals/Homeworks/11.Regular-Expressions_Lab/P02_MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,22 @@
+﻿
+namespace P02_MatchPhoneNumber
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class MatchPhoneNumber
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var regex = new Regex(@"(\+\d{3}\s\d\s\d{3}\s\d{4})\b|(\+\d{3}-\d-\d{3}-\d{4})\b");
+            var matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                Console.Write(string.Join(", ", match.Value));
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ TechFundamentals/Homeworks/11.Regular-Expressions_Lab/P03_MatchDates/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace P03_MatchDates
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            var regex = @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            MatchCollection dates = Regex.Matches(text, regex);
+            foreach (Match date in dates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
