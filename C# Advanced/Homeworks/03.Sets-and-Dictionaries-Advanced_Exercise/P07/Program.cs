e3840ad7865791445636cc4aecb2343a7b8a5c4c
Philip Shishov <philshishov@gmail.com>
2022-04-09 12:14:56 +0200

    Sets and dictionaries * additional exercise

---
+++ C# Advanced/Homeworks/03.Sets-and-Dictionaries-Advanced_Exercise/P07/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new Dictionary<string, HashSet<string>>();
+            var followers = new Dictionary<string, HashSet<string>>();
+
+            string command = "";
+
+            while ((command = Console.ReadLine()) != "Statistics")
+            {
+                string[] tokens = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string username = tokens[0];
+                string commandType = tokens[1];
+
+                if (commandType == "joined")
+                {
+                    if (users.ContainsKey(username) == false)
+                    {
+                        users.Add(username, new HashSet<string>());
+                        followers.Add(username, new HashSet<string>());
+                    }
+                }
+                else if (commandType == "followed")
+                {
+                    string followedUsername = tokens[2];
+
+                    if (users.ContainsKey(username) && username != followedUsername)
+                    {
+                        users[username].Add(followedUsername);
+                        followers[followedUsername].Add(username);
+                    }
+                }
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {users.Count} vloggers in its logs.");
+            int rank = 1;
+
+            foreach (var user in users.OrderByDescending(x => followers[x.Key].Count).ThenBy(x => x.Value.Count))
+            {
+                if (rank == 1)
+                {
+                    Console.WriteLine($"{rank++}. {user.Key} : {followers[user.Key].Count} followers, {user.Value.Count} following");
+                    foreach (var follower in followers[user.Key].OrderBy(x => x))
+                    {
+                        Console.WriteLine($"*  {follower}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{rank++}. {user.Key} : {followers[user.Key].Count} followers, {user.Value.Count} following");
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Homeworks/03.Sets-and-Dictionaries-Advanced_Exercise/P08/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contests = new Dictionary<string, string>();
+            var totalPoints = new Dictionary<string, int>();
+            var participants = new SortedDictionary<string, Dictionary<string, int>>();
+
+            string command = "";
+
+            while ((command = Console.ReadLine()) != "end of contests")
+            {
+                string[] tokens = command.Split(':');
+                contests.Add(tokens[0], tokens[1]);
+            }
+
+            while ((command = Console.ReadLine()) != "end of submissions")
+            {
+                string[] tokens = command.Split("=>");
+                string contestName = tokens[0];
+                string password = tokens[1];
+                string username = tokens[2];
+                int points = int.Parse(tokens[3]);
+
+                if (contests.ContainsKey(contestName) && contests[contestName] == password)
+                {
+                    if (!participants.ContainsKey(username))
+                    {
+                        participants.Add(username, new Dictionary<string, int>());
+                        totalPoints.Add(username, 0);
+                    }
+
+                    if (!participants[username].ContainsKey(contestName))
+                    {
+                        participants[username].Add(contestName, points);
+                        totalPoints[username] += points;
+                    }
+                    else
+                    {
+                        int oldPoints = participants[username][contestName];
+
+                        if (oldPoints < points)
+                        {
+                            participants[username][contestName] = points;
+                            totalPoints[username] += points - oldPoints;
+                        }
+                    }
+                }
+
+            }
+
+            var bestParticipant = totalPoints
+                .OrderByDescending(x => x.Value)
+                .FirstOrDefault();
+
+            Console.WriteLine($"Best candidate is {bestParticipant.Key} with total {bestParticipant.Value} points.");
+
+            Console.WriteLine("Ranking:");
+
+            foreach (var participant in participants)
+            {
+                Console.WriteLine(participant.Key);
+
+                foreach (var contest in participant.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
+                }
+            }
+        }
+    }
+}
