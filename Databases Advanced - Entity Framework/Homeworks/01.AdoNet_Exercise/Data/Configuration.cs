cbcb945b933a51d5e9b8d0fa57ed64e590b6dab7
Philip Shishov <philshishov@gmail.com>
2022-05-16 10:26:16 +0300

    AdoNet * refactor files and structure

---
+++ Databases Advanced - Entity Framework/Homeworks/01.AdoNet_Exercise/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = 
+            "Server=.\\SQL2019;Database=MinionsDB;Integrated Security=True";
+    }
+}
---
+++ Databases Advanced - Entity Framework/Homeworks/01.AdoNet_Exercise/P02/StartUp.cs
@@ -0,0 +1,41 @@
+﻿
+namespace P02_VillainNames
+{
+    using System;
+    using System.Data.SqlClient;
+
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var connection = new SqlConnection(Configuration.ConnectionString))
+            {
+                connection.Open();
+
+                string sqlQuery = @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                    FROM Villains AS v 
+                                    JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                GROUP BY v.Id, v.Name 
+                                  HAVING COUNT(mv.VillainId) > 3 
+                                ORDER BY COUNT(mv.VillainId)";
+
+                using (var command = new SqlCommand(sqlQuery, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string name = (string)reader["Name"]; //reader[0];
+                            int count = (int)reader[1];
+
+                            Console.WriteLine($"{name} - {count}");
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
