055026978cc76a9e855ae5a34a99e3a89fa6eecb
Philip Shishov <philshishov@gmail.com>
2022-05-21 08:08:48 +0200

    Advanced Relations * net6

--- Databases Advanced - Entity Framework/Homeworks/06.Advanced-Relations_Exercise/BillsPaymentSystem.Data/Configuration.cs
+++ Databases Advanced - Entity Framework/Homeworks/06.Advanced-Relations_Exercise/BillsPaymentSystem.Data/Configuration.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace BillsPaymentSystem.Data
 {
     internal static class Configuration
     {
-        internal static string ConnectionString => "Server=.\\SQLEXPRESS;Database=MyBillsPaymentSystem;Integrated Security=True;";
+        internal static string ConnectionString => "Server=.\\SQL2019;Database=MyBillsPaymentSystem;Integrated Security=True;";
     }
 }
